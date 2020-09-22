b024991d38f23a6dfeaf88c9cc39505df64e58e9
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-09-22 15:08:43 +0530

    map State/Dispatch To props -Video 259

--- redux-basics.js
+++ redux-basics.js
@@ -5,15 +5,43 @@ initialState = {
     counter: 0
 }
 
+
 //Reducer
+
+
 const rootReducer = (state = initialState, action) => {
+    if (action.type == 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        };
+    }
+    if (action.type == 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
     return state;
 };
 
+
 //Store
+
+
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-//Dispatching action
 
 //Subscription
+
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+})
+
+//Dispatching action
+
+
+store.dispatch({ type: 'INC_COUNTER' })
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+console.log(store.getState());
