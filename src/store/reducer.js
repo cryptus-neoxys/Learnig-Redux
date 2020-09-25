a559b603593b21ca7aca29bcb3da9fb0d496fb1d
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-09-25 14:47:24 +0530

    State and Change Handlers replaced with ReactRedux

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,12 +3,30 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        }
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        case 'DECREMENT':
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+        case 'ADD':
+            return {
+                ...state,
+                counter: state.counter + action.val
+            }
+        case 'SUBTRACT':
+            return {
+                ...state,
+                counter: state.counter - action.val
+            }
+        default:
+            return state
     }
-    return state;
 }
 
 export default reducer;
